e8308e92c6789af83ba7c10ab59855014c058af7
caritogcdev <carito940905@gmail.com>
2024-09-20 06:41:28 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   Patterns Design
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
